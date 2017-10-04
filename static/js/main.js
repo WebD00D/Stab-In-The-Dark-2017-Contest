44059025d4ce57ca84c99e31af0d71de90469cf5
Christian Bryant <christian.bryant@mobelux.com>
2017-10-04 14:21:19 -0400

    updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,66 @@
 window.addEventListener("load", function(event) {
+  const submitEmailBtn = document.getElementById("js_submitEmail");
+  submitEmailBtn.onclick = function() {
+    const email = document.getElementById("email").value;
 
-    const submitEmailBtn = document.getElementById("js_submitEmail")
-    submitEmailBtn.onclick = function(){
+    // TODO: Save to Firebase DB, send to Node backend where we will send to
+    // Campaign Monitor.
+    console.log(email);
 
-      const email = document.getElementById("email").value;
+    fetch("/sumbit-email", {
+      method: "post",
+      body: JSON.stringify({ email: email }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    });
 
-      // TODO: Save to Firebase DB, send to Node backend where we will send to
-      // Campaign Monitor.
+    // TODO: Set Cookie, or Data Attribue with submitt email, so
+    // we can reference when submitting the poll.
+    document
+      .getElementsByClassName("page")[0]
+      .setAttribute("data-email", email);
 
-      // TODO: Set Cookie, or Data Attribue with submitte email, so
-      // we can reference when submitting the poll.
-      document.getElementsByClassName("page")[0].setAttribute("data-email", email);
+    // TODO: Show Survey.
 
-      // TODO: Show Survey.
+    document
+      .getElementsByClassName("js_section_one")[0]
+      .classList.add("d-none");
+    document
+      .getElementsByClassName("js_section_two")[0]
+      .classList.remove("d-none");
+  };
 
-      document.getElementsByClassName("js_section_one")[0].classList.add("d-none");
-      document.getElementsByClassName("js_section_two")[0].classList.remove("d-none");
+  const submitEntry = document.getElementById("js_submitEntry");
+  submitEntry.onclick = function() {
+    // TODO: Grab selected shaper and Email
+    const email = document
+      .getElementsByClassName("page")[0]
+      .getAttribute("data-email");
+    const shaper = document.querySelector('input[name = "shaper"]:checked')
+      .value;
+
+    // Save to Firebase DB
+    // Are we going to submit to a polling result place?
+    fetch("/sumbit-survey", {
+      method: "post",
+      body: JSON.stringify({ email: email, shaper: shaper }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    });
 
 
-    }
 
-    const submitEntry = document.getElementById("js_submitEntry")
-    submitEntry.onclick = function(){
+    // TODO: Show Success
 
-      // TODO: Grab selected shaper and Email
-
-      const email = document.getElementsByClassName("page")[0].getAttribute("data-email");
-      const shaper = document.querySelector('input[name = "shaper"]:checked').value;
-      alert(email + ' ' + shaper );
-
-    }
-
-
-
-
+    document
+      .getElementsByClassName("js_section_two")[0]
+      .classList.add("d-none");
+    document
+      .getElementsByClassName("js_section_three")[0]
+      .classList.remove("d-none");
+  };
 });
