faf4eddca1974eca827c62c997c3dff719f252dc
Christian Bryant <christian.bryant@mobelux.com>
2017-10-04 20:53:39 -0400

    just updating the code

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,23 @@
-
 window.addEventListener("load", function(event) {
   const submitEmailBtn = document.getElementById("js_submitEmail");
   submitEmailBtn.onclick = function() {
     const email = document.getElementById("email").value;
+    document.getElementById("errormessage").classList.add("d-none");
 
-    // TODO: Save to Firebase DB, send to Node backend where we will send to
-    // Campaign Monitor.
+    if (!validateEmail(email)) {
+      document.getElementById("errormessage").classList.remove("d-none");
+      return;
+    }
 
-      $.ajax({
-        method: "POST",
-        url: "/sumbit-email",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        data: JSON.stringify({ email: email })
-      }).done(function(msg) {
-        console.log("done");
-      });
-
+    $.ajax({
+      method: "POST",
+      url: "/sumbit-email",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      data: JSON.stringify({ email: email })
+    }).done(function(msg) {
+      console.log("done");
+    });
 
     // TODO: Set Cookie, or Data Attribue with submitt email, so
     // we can reference when submitting the poll.
@@ -33,6 +34,11 @@ window.addEventListener("load", function(event) {
       .getElementsByClassName("js_section_two")[0]
       .classList.remove("d-none");
   };
+
+  function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
 
   const submitEntry = document.getElementById("js_submitEntry");
   submitEntry.onclick = function() {
@@ -63,17 +69,39 @@ window.addEventListener("load", function(event) {
       .classList.remove("d-none");
   };
 
+  document.getElementById("js_northernHemi").onclick = function() {
+    document
+      .getElementsByClassName("js_section_hemisphere")[0]
+      .classList.add("d-none");
+    document
+      .getElementsByClassName("js_section_one")[0]
+      .classList.remove("d-none");
+    document
+      .getElementsByClassName("page")[0]
+      .setAttribute("data-hemisphere", "Northern");
+  };
 
-  document.getElementsByClassName("js-mail")[0].onclick = function(){
+  document.getElementById("js_southernHemi").onclick = function() {
+    document
+      .getElementsByClassName("js_section_hemisphere")[0]
+      .classList.add("d-none");
+    document
+      .getElementsByClassName("js_section_one")[0]
+      .classList.remove("d-none");
+    document
+      .getElementsByClassName("page")[0]
+      .setAttribute("data-hemisphere", "Southern");
+  };
+
+  document.getElementsByClassName("js-mail")[0].onclick = function() {
     document.getElementById("lnkEmail").click();
-  }
+  };
 
-  document.getElementsByClassName("js-tweet")[0].onclick = function(){
+  document.getElementsByClassName("js-tweet")[0].onclick = function() {
     document.getElementById("lnkTwitter").click();
-  }
+  };
 
-  document.getElementsByClassName("js-share-fb")[0].onclick = function(){
+  document.getElementsByClassName("js-share-fb")[0].onclick = function() {
     document.getElementById("lnkFacebook").click();
-  }
-
+  };
 });
